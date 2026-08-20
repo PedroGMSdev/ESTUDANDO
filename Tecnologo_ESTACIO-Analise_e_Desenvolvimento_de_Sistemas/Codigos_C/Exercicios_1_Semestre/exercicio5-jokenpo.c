7bd5c4fd0f224830c77d5e641344607be4426be0
Pedro Guilherme Martins <pedroguilherme_ms@yahoo.com.br>
2026-08-20 12:38:03 -0300

    Novo Exercício no Tecnólogo

---
+++ Tecnologo_ESTACIO-Analise_e_Desenvolvimento_de_Sistemas/Codigos_C/Exercicios_1_Semestre/exercicio5-jokenpo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+    int escolhaJogador, escolhaComputador;
+    srand(time());
+
+    printf("---------------------------------");
+    printf("PEDRA, PAPEL E TESOURA");
+    printf("---------------------------------");
+    
+}
