a00dd577079918c5a4274914296b833cfe64063a
Pedro Guilherme Martins <pedroguilherme_ms@yahoo.com.br>
2026-09-06 21:27:03 -0300

    Desafio 10 Cumprido no Tecnólogo

--- Tecnologo_ESTACIO-Analise_e_Desenvolvimento_de_Sistemas/Codigos_C/Praticas_1_Semestre/desafio10.c
+++ Tecnologo_ESTACIO-Analise_e_Desenvolvimento_de_Sistemas/Codigos_C/Praticas_1_Semestre/desafio10.c
@@ -1,22 +1,54 @@
-// DESAFIO Batalha Naval: Nível Mestre
+// DESAFIO Batalha Naval: Nível Novato
 // Cadeira: Introdução à Programação de Computadores
-// Módulo: Batalha Naval com Vetores e Matrizes
+// Módulo: Jogo de Batalha Naval com Vetores e Matrizes
 
 #include<stdio.h>
 
 int main(){
 
     int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0,}
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
+
+    int navio1 = 3;
+    int navio2 = 3;
+
+    tabuleiro[7][6] = navio1;
+    tabuleiro[7][7] = navio1;
+    tabuleiro[7][8] = navio1;
+
+    tabuleiro[4][2] = navio2;
+    tabuleiro[5][2] = navio2;
+    tabuleiro[6][2] = navio2;
+
+    int linha = 0;
+    int coluna = 0;
+    int numeroLinha = 1;
+
+    printf("TABULEIRO DE BATALHA NAVAL\n");
+    printf("--------------------------\n");
+    printf("A - B - C - D - E - F - G - H - I - J\n");
+    printf("|   |   |   |   |   |   |   |   |   |\n");
+
+    for (int i = 0; i < 10; i++){
+
+        for (int j = 0; j < 10; j++){
+            printf("%d - ", tabuleiro[linha][coluna]);
+            coluna++;
+        }
+
+        printf(" (%d)", numeroLinha);
+        numeroLinha++;
+        printf("\n");
+    }
 
 }
